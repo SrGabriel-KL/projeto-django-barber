e8dffa72fb44f6cf2d643ef56bba961320ede146
SrGabriel-KL <gabrielsenasena234@gmail.com>
2026-01-30 01:51:08 -0300

    Meu-Site-Django-Barbearia

---
+++ Site-Receitas-atualizado-main/recipes/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
+
+
+def home (request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    return render (request,'recipes/pages/home.html', context={
+        'recipes': recipes,
+
+    })
+    
+def category (request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id,is_published=True).order_by('-id')
+    return render (request,'recipes/pages/category.html', context={
+        'recipes': recipes,
+        
+    })
+
+def receita(request, id):
+    recipe = Recipe.objects.get(id=id)
+    return render (request,'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
+    })
+    
+    
+
+
