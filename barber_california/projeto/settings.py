7fe03887d522f3ea5519557fb4ef9526d5eb0890
SrGabriel-KL <gabrielsenasena234@gmail.com>
2026-01-30 22:15:47 -0300

    atualizando

--- barber_california/projeto/settings.py
+++ barber_california/projeto/settings.py
@@ -123,12 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
-    
 ]
-STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_URL = '/media/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
